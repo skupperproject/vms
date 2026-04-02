6488aa23743e8d4561b52c4dfdbf986d387758ed
JPadovano1483 <james.padovano@ibm.com>
2026-04-02 13:52:13 -0400

    added logout button

--- console/src/components/Header/Header.js
+++ console/src/components/Header/Header.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import {
   Header,
   HeaderName,
   HeaderGlobalBar,
   HeaderGlobalAction,
+  OverflowMenu,
+  OverflowMenuItem,
 } from '@carbon/react';
 import { UserAvatar, Notification } from '@carbon/icons-react';
 
 const AppHeader = () => {
+  const [displayName, setDisplayName] = useState('');
+
+  useEffect(() => {
+    fetchDisplayName();
+  }, []);
+
+  const fetchDisplayName = async () => {
+    try {
+      const response = await fetch('/api/v1alpha1/user/profile');
+      const data = await response.json();
+      setDisplayName(data.name);
+    } catch (error) {
+      console.error('Error fetching display name:', error);
+    }
+  };
+
+  const handleLogout = useCallback(() => {
+    window.location.assign('/logout');
+  }, []);
+
   return (
     <Header aria-label="SkupperVMS">
       <HeaderName href="/" prefix="">
@@ -21,10 +43,16 @@ const AppHeader = () => {
           <Notification size={20} />
         </HeaderGlobalAction>
         <HeaderGlobalAction
-          aria-label="User Avatar"
+          aria-label="User Menu"
           tooltipAlignment="end"
         >
-          <UserAvatar size={20} />
+          <OverflowMenu renderIcon={() => <UserAvatar size={20} />} flipped>
+            <OverflowMenuItem style={{ color: 'black' }} itemText={displayName} disabled />
+            <OverflowMenuItem
+              itemText="Logout"
+              onClick={handleLogout}
+            />
+          </OverflowMenu>
         </HeaderGlobalAction>
       </HeaderGlobalBar>
     </Header>
